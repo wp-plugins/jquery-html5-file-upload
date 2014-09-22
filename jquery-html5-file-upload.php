aa5c573ad1ca511e849e348a580403571404573a
AnwarSwabiri <AnwarSwabiri@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-22 18:56:18 +0000

    fixing the developer name
    git-svn-id: https://plugins.svn.wordpress.org/jquery-html5-file-upload/tags/2.1@994909 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jquery-html5-file-upload.php
+++ jquery-html5-file-upload.php
@@ -4,7 +4,7 @@ Plugin Name: JQuery Html5 File Upload
 Plugin URI: http://wordpress.org/extend/plugins/jquery-html5-file-upload/
 Description: This plugin adds a file upload functionality to the front-end screen. It allows multiple file upload asynchronously along with upload status bar.
 Version: 2.1
-Author: Anwar Swabiri
+Author: Sinash Shajahan
 Author URI: 
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
